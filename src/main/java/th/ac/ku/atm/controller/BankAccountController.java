bf3d005bcd42d990301e309d6d0675e374369967
Nae-nwp <Nawapon.c@ku.th>
2021-01-31 17:08:55 +0700

    8.2 past2

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -2,10 +2,7 @@ package th.ac.ku.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import th.ac.ku.atm.model.BankAccount;
 import th.ac.ku.atm.service.BankAccountService;
 
@@ -28,8 +25,7 @@ public class BankAccountController {
     @PostMapping
     public String registerBankAccount(@ModelAttribute BankAccount bankAccount, Model model) {
         bankAccountService.createBankAccount(bankAccount);
-
-        //model.addAttribute("allBankAccount", bankAccountService.getBankAccount()) ;
+        model.addAttribute("allBankAccount", bankAccountService.getBankAccount()) ;
         return "redirect:bankaccount" ;
     }
 
